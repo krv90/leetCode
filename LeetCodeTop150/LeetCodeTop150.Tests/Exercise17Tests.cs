30d1b6c4976ea1da057ff0aea0bc53f38d3e6e5e
Козлов Роман <kozlov@moedelo.org>
2024-01-15 16:58:00 +0300

    17. Letter Combinations of a Phone Number

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise17Tests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise17Tests
+{
+    [Test]
+    public void Case1()
+    {
+        Exercise17.LetterCombinations("23")
+            .Should()
+            .BeEquivalentTo(new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" });
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        Exercise17.LetterCombinations("2")
+            .Should()
+            .BeEquivalentTo(new[] { "a", "b", "c" });
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise17.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 17. Letter Combinations of a Phone Number
+/// </summary>
+/// <see cref="https://habr.com/ru/companies/otus/articles/746408/"/>
+public static class Exercise17
+{
+    private static readonly Dictionary<int, string[]> CharsByDigit = new Dictionary<int, string[]>
+    {
+        { '2', new[] { "a", "b", "c" } },
+        { '3', new[] { "d", "e", "f" } },
+        { '4', new[] { "g", "h", "i" } },
+        { '5', new[] { "j", "k", "l" } },
+        { '6', new[] { "m", "n", "o" } },
+        { '7', new[] { "p", "q", "r", "s" } },
+        { '8', new[] { "t", "u", "v" } },
+        { '9', new[] { "w", "x", "y", "z" } }
+    };
+
+    public static IList<string> LetterCombinations(string digits)
+    {
+        if (digits?.Any() != true)
+            return Array.Empty<string>();
+
+        var result = new List<string>(CharsByDigit[digits[0]]);
+
+        for (var i = 1; i < digits.Length; i++)
+        {
+            result = result.Join(
+                    CharsByDigit[digits[i]],
+                    _ => true,
+                    _ => true,
+                    (r, c) => r + c)
+                .ToList();
+        }
+
+        return result;
+    }
+}
