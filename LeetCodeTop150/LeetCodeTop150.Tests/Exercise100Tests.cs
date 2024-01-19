0aa5e370203059f7dd3ab540888709aecaafd6c3
Козлов Роман <kozlov@moedelo.org>
2024-01-19 17:57:52 +0300

    100. Same Tree
    *ручное наивное вычисление тестового дерева

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise100Tests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise100Tests
+{
+    [Test]
+    public void Case1()
+    {
+        var p = CreateNode(1, 2, 3);
+        var q = CreateNode(1, 2, 3);
+
+        Exercise100.IsSameTree(p, q).Should().Be(true);
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        var p = CreateNode(1, 2);
+        var q = CreateNode(1, null, 2);
+
+        Exercise100.IsSameTree(p, q).Should().Be(false);
+    }
+    
+    [Test]
+    public void Case3()
+    {
+        var p = CreateNode(1, 2, 1);
+        var q = CreateNode(1, 1, 2);
+
+        Exercise100.IsSameTree(p, q).Should().Be(false);
+    }
+    
+    [Test]
+    public void Case4()
+    {
+        var p = CreateNode(10, 5, 15);
+        var q = CreateNode(10, 5, null);
+        q.left = CreateNode(5, null, 15);
+
+        Exercise100.IsSameTree(p, q).Should().Be(false);
+    }
+
+    private static Exercise100.TreeNode CreateNode(params int?[] values)
+    {
+        var start = values.First();
+        var result = new Exercise100.TreeNode(start!.Value);
+
+        for (var i = 1; i < values.Length; i++)
+        {
+            var nextNode = values[i] == null ? null : new Exercise100.TreeNode(values[i].Value);
+            if (int.IsOddInteger(i))
+            {
+                result.left = nextNode;
+            }
+            else
+            {
+                result.right = nextNode;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise100.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 100. Same Tree
+/// </summary>
+public static class Exercise100
+{
+    public static bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == null && q == null)
+        {
+            return true;
+        }
+
+        if (p?.val != q?.val)
+        {
+            return false;
+        }
+
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
