0053103b6be7d853dc7cb8d65f0e6d09ba011b2a
Козлов Роман <kozlov@moedelo.org>
2024-01-23 16:20:08 +0300

    53. Maximum Subarray (алгоритма Кадане)

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise53Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise53Tests
+{
+    [Test]
+    public void Case1()
+    {
+        // subarray [4,-1,2,1]
+        Exercise53.MaxSubArraySum(new[] { -2, 1, -3, 4, -1, 2, 1, -5 })
+            .Should()
+            .Be(6);
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        // subarray [1]
+        Exercise53.MaxSubArraySum(new[] { 1 })
+            .Should()
+            .Be(1);
+    }
+    
+    [Test]
+    public void Case3()
+    {
+        // subarray [5,4,-1,7,8]
+        Exercise53.MaxSubArraySum(new[] { 5,4,-1,7,8 })
+            .Should()
+            .Be(23);
+    }
+
+    [Test]
+    public void Case4()
+    {
+        // subarray [1]
+        Exercise53.MaxSubArraySum(new[] { -2, 1 })
+            .Should()
+            .Be(1);
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise53.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 53. Maximum Subarray
+/// </summary>
+public static class Exercise53
+{
+    public static int MaxSubArraySum(int[] nums)
+    {
+        var currentSum = nums[0];
+        var maxSum = currentSum;
+        
+        for (int i = 1; i < nums.Length; i++)
+        {
+            currentSum += nums[i];
+
+            if (currentSum < nums[i])
+                currentSum = nums[i];
+            
+            if (maxSum < currentSum) 
+                maxSum = currentSum;
+        }
+
+        return maxSum;
+    }
+}
