454210fb2c51e3127c63a2093f45d24c82d6f872
Козлов Роман <kozlov@moedelo.org>
2024-02-08 19:36:31 +0300

    637. Average of Levels in Binary Tree (через словарь)

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise637Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise637Tests
+{
+    [Test]
+    public void Case1()
+    {
+        var root = new Exercise637.TreeNode(3)
+        {
+            left = new Exercise637.TreeNode(9),
+            right = new Exercise637.TreeNode(20)
+            {
+                left = new Exercise637.TreeNode(15),
+                right = new Exercise637.TreeNode(7)
+            }
+        };
+
+        Exercise637.AverageOfLevels(root).Should().BeEquivalentTo(new[] { 3.00000, 14.50000, 11.00000 });
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        var root = new Exercise637.TreeNode(3)
+        {
+            left = new Exercise637.TreeNode(9)
+            {
+                left = new Exercise637.TreeNode(15),
+                right = new Exercise637.TreeNode(7)
+            },
+            right = new Exercise637.TreeNode(20)
+        };
+        Exercise637.AverageOfLevels(root).Should().BeEquivalentTo(new[] { 3.00000, 14.50000, 11.00000 });
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise637.cs
@@ -0,0 +1,62 @@
+namespace LeetCodeTop150;
+
+/// <summary>
+/// 637. Average of Levels in Binary Tree
+/// </summary>
+public static class Exercise637
+{
+    public static IList<double> AverageOfLevels(TreeNode root) {
+        /* по условию всегда есть
+        if (root == null)
+        {
+            return new List<double>(0);
+        }
+        */
+
+        var levelMap = new Dictionary<int, LevelSummary>();
+        CalcToLevelSummary(root, levelMap, 1);
+
+        return levelMap.Values.Select(s => s.Sum / s.Count).ToArray();
+    }
+
+    private static void CalcToLevelSummary(TreeNode node, Dictionary<int, LevelSummary> levelMap, int level)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        var summary = levelMap.GetValueOrDefault(level);
+        summary += new LevelSummary(node);
+        levelMap[level] = summary;
+        
+        CalcToLevelSummary(node.left, levelMap, level + 1);
+        CalcToLevelSummary(node.right, levelMap, level + 1);
+    }
+
+    private struct LevelSummary
+    {
+        public LevelSummary(TreeNode node)
+        {
+            Sum = node.val;
+            Count = 1;
+        }
+        public double Sum { get; set; }
+        public double Count { get; set; }
+        public static LevelSummary operator +(LevelSummary a, LevelSummary b) => new LevelSummary { Sum = a.Sum + b.Sum, Count = a.Count + b.Count };
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
