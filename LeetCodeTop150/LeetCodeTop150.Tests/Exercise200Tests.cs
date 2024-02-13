3de83b6dff9d9ed77def1cb6db1b5c86a4597c7f
Козлов Роман <kozlov@moedelo.org>
2024-02-13 22:26:33 +0300

    200. Number of Islands

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise200Tests.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise200Tests
+{
+    [Test]
+    public void Case1()
+    {
+        var grid = new char[][]
+        {
+            new[] { '1', '1', '1', '1', '0' },
+            new[] { '1', '1', '0', '1', '0' },
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '0', '0', '0', '0', '0' }
+        };
+
+        Exercise200.NumIslands(grid).Should().Be(1);
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        var grid = new char[][]
+        {
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '0', '0', '1', '0', '0' },
+            new[] { '0', '0', '0', '1', '1' }
+        };
+
+        Exercise200.NumIslands(grid).Should().Be(3);
+    }
+    
+    [Test]
+    public void Case3()
+    {
+        var grid = new char[][]
+        {
+            new[] { '0', '0', '0', '0', '0' },
+            new[] { '0', '1', '1', '1', '0' },
+            new[] { '0', '1', '1', '1', '0' },
+            new[] { '0', '1', '1', '1', '0' },
+            new[] { '0', '0', '0', '0', '0' }
+        };
+
+        Exercise200.NumIslands(grid).Should().Be(1);
+    }
+    
+    [Test]
+    public void Case4()
+    {
+        var grid = new char[][]
+        {
+            new[] { '0', '1', '1', '0', '0' },
+            new[] { '0', '0', '1', '1', '0' },
+            new[] { '0', '0', '0', '1', '0' },
+            new[] { '1', '0', '0', '1', '0' },
+            new[] { '0', '1', '1', '1', '0' }
+        };
+
+        Exercise200.NumIslands(grid).Should().Be(2);
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise200.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 200. Number of Islands
+/// </summary>
+public static class Exercise200
+{
+    public static int NumIslands(char[][] grid)
+    {
+        /* по условиям задачи всегда непустой
+        if (grid.Length == 0)
+        {
+            return 0;
+        }
+        */
+        
+        var result = 0;
+        bool [,] visited = new bool[grid.Length, grid[0].Length];
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (visited[i, j])
+                {
+                    continue;
+                }
+                
+                result += DetectIsland(grid, i, j, visited);
+            }
+        }
+
+        return result;
+    }
+
+    private static int DetectIsland(char[][] grid, int i, int j, bool[,] visited)
+    {
+        var isValid = 0 <= i && i < grid.Length
+            && 0 <= j && j < grid[i].Length
+            && !visited[i, j];
+        
+        if (!isValid)
+        {
+            return 0;
+        }
+
+        visited[i, j] = true;
+        if (grid[i][j] == '0')
+        {
+            return 0;
+        }
+
+        DetectIsland(grid, i + 1, j, visited);
+        DetectIsland(grid, i - 1, j, visited);
+        DetectIsland(grid, i, j + 1, visited);
+        DetectIsland(grid, i, j - 1, visited);
+        
+        return 1;
+    }
+}
