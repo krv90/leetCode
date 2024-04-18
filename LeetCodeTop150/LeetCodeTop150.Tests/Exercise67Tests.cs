c8d72398ec48362521486be8f3226168bb299018
Козлов Роман <kozlov@moedelo.org>
2024-04-18 17:31:51 +0300

    67. Add Binary

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise67Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise67Tests
+{
+    [Test]
+    [TestCase("11", "1", "100")]
+    [TestCase("1010", "1011", "10101")]
+    [TestCase("111", "1111", "10110")]
+    [TestCase("0", "0", "0")]
+    public void Cases(string a, string b, string expected)
+    {
+        Exercise67.AddBinary(a, b).Should().Be(expected);
+    }
+
+    [Test]
+    public void NoExceptionCase()
+    {
+        var action = () => Exercise67.AddBinary(
+            "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+            "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+        );
+
+        action.Should().NotThrow();
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise67.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace LeetCodeTop150;
+
+/// <summary>
+/// 67. Add Binary
+/// </summary>
+public static class Exercise67
+{
+    public static string AddBinary(string a, string b)
+    {
+        /*
+         var res = Convert.ToInt32(a, 2) + Convert.ToInt32(b, 2);
+         return Convert.ToString(res, 2);
+         */
+        var builder = new StringBuilder(Math.Max(a.Length, b.Length) + 1);
+
+        var indexA = a.Length - 1;
+        var indexB = b.Length - 1;
+        var plus1 = false;
+
+        while (indexA >= 0 || indexB >= 0)
+        {
+            var ai = indexA >= 0 && a[indexA] == '1';
+            indexA--;
+
+            var bi = indexB >= 0 && b[indexB] == '1';
+            indexB--;
+
+            var resChar = ai ^ bi ^ plus1 ? '1' : '0';
+            builder.Insert(0, resChar);
+            
+            plus1 = ai & bi || (ai | bi) & plus1;
+        }
+
+        if (plus1)
+        {
+            builder.Insert(0, '1');
+        }
+
+        return builder.ToString();
+    }
+}
