aea92f023972ba67fb582a6aa06dc6ff39d038df
Козлов Роман <kozlov@moedelo.org>
2024-06-11 13:03:24 +0300

    605. Can Place Flowers

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise605Tests.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise605Tests
+{
+    [Test]
+    [TestCase(1, true)]
+    [TestCase(2, false)]
+    public void Case_1_and_2(int n, bool expected)
+    {
+        Exercise605
+            .CanPlaceFlowers(new[] { 1, 0, 0, 0, 1 }, n)
+            .Should()
+            .Be(expected);
+    }
+    
+    [Test]
+    [TestCase(0, true)]
+    [TestCase(1, false)]
+    public void Case_3_4(int val, bool expected)
+    {
+        Exercise605
+            .CanPlaceFlowers(new[] { val }, 1)
+            .Should()
+            .Be(expected);
+    }
+    
+    [Test]
+    public void Case_5()
+    {
+        Exercise605
+            .CanPlaceFlowers(new[] { 0, 0, 1 }, 1)
+            .Should()
+            .Be(true);
+    }
+    
+    [Test]
+    public void Case_6()
+    {
+        Exercise605
+            .CanPlaceFlowers(new[] { 1, 0, 0 }, 1)
+            .Should()
+            .Be(true);
+    }
+    
+    [Test]
+    public void Case_7()
+    {
+        Exercise605
+            .CanPlaceFlowers(new[] { 1, 0, 1, 0, 1 }, 1)
+            .Should()
+            .Be(false);
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise605.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 605. Can Place Flowers
+/// </summary>
+/// <remarks>
+/// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+/// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+/// </remarks>
+public static class Exercise605
+{
+    public static bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        var prevEmpty = true;
+        var canBePlantedPlaces = 0;
+
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            var nextEmpty = i == flowerbed.Length - 1 || flowerbed[i + 1] == 0;
+            
+            if (flowerbed[i] == 0)
+            {
+                if (prevEmpty && nextEmpty)
+                {
+                    canBePlantedPlaces++;
+                    prevEmpty = false;
+                }
+                else
+                {
+                    prevEmpty = true;
+                }
+            }
+            else
+            {
+                prevEmpty = false;
+            }
+
+            if (canBePlantedPlaces == n)
+            {
+                return true;
+            }
+        }
+
+        return canBePlantedPlaces >= n;
+    }
+}
