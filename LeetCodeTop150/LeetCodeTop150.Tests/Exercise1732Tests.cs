f7db497104347add6b65161bedee37c7e8bd2ecb
Козлов Роман <kozlov@moedelo.org>
2024-06-11 15:34:20 +0300

    1732. Find the Highest Altitude

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise1732Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise1732Tests
+{
+    [Test]
+    public void Case1()
+    {
+        Exercise1732.LargestAltitude(new[] { -5, 1, 5, 0, -7 }).Should().Be(1);
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        Exercise1732.LargestAltitude(new[] { -4, -3, -2, -1, 4, 3, 2 }).Should().Be(0);
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise1732.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 1732. Find the Highest Altitude
+/// </summary>
+public static class Exercise1732
+{
+    public static int LargestAltitude(int[] gain) {
+        var max = 0;
+        var current = 0;
+        foreach(var i in gain){
+            current += i;
+            if (max < current){
+                max = current;
+            }
+        }
+
+        return max;
+    }
+}
