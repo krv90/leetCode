695a24b35bf3c52d6ccba78caa37f34397665654
Козлов Роман <kozlov@moedelo.org>
2024-06-17 18:17:44 +0300

    108. Convert Sorted Array to Binary Search Tree
    неоптимальное, но быстрое в реализации и простое в понимании решение

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise108Tests.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise108Tests
+{
+    [Test]
+    public void Case1()
+    {
+        Exercise108
+            .SortedArrayToBST(-10, -3, 0, 5, 9)
+            .Should()
+            .BeEquivalentTo(
+                CreateBST(0, -3, 9, -10, null, 5),
+                opt => opt.WithStrictOrdering()
+            );
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        Exercise108
+            .SortedArrayToBST(1,3)
+            .Should()
+            .BeEquivalentTo(
+                CreateBST(3, 1),
+                opt => opt.WithStrictOrdering()
+            );
+    }
+
+    private static Exercise108.TreeNode CreateBST(params int?[] arr)
+    {
+        var top = new Exercise108.TreeNode(arr[0].GetValueOrDefault());
+        var nodes = new Queue<Exercise108.TreeNode>();
+        nodes.Enqueue(top);
+        
+        var i = 0;
+        do
+        {
+            var node = nodes.Dequeue();
+            var left = arr.ElementAtOrDefault(++i);
+            if (left != null)
+            {
+                node.left = new Exercise108.TreeNode(left.Value);
+                nodes.Enqueue(node.left);
+            }
+            
+            var right = arr.ElementAtOrDefault(++i);
+            if (right != null)
+            {
+                node.right = new Exercise108.TreeNode(right.Value);
+                nodes.Enqueue(node.right);
+            }
+
+        } while (i < arr.Length);
+
+        return top;
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise108.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 108. Convert Sorted Array to Binary Search Tree
+/// </summary>
+/// <remarks>
+/// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+/// </remarks>
+public class Exercise108
+{
+    public static TreeNode SortedArrayToBST(params int[] nums)
+    {
+        return GetMiddle(nums);
+    }
+
+    private static TreeNode GetMiddle(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return null;
+        }
+        
+        if (nums.Length == 1)
+        {
+            return new TreeNode(nums[0]);
+        }
+        
+        var middleIndex = nums.Length / 2;
+        var treeNode = new TreeNode(nums[middleIndex])
+        {
+            left = GetMiddle(nums.Skip(0).Take(middleIndex).ToArray()),
+            right = GetMiddle(nums.Skip(middleIndex + 1).Take(middleIndex).ToArray())
+        };
+
+        return treeNode;
+    }
+
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
