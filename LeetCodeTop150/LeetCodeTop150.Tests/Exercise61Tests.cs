e75776340f8596d396e7ed55d8d61e8e94e77d42
Козлов Роман <kozlov@moedelo.org>
2024-06-28 18:56:40 +0300

    61. Rotate List

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise61Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise61Tests
+{
+    [Test]
+    public void Case1()
+    {
+        Exercise61
+            .RotateRight(Exercise61.ListNode.FromArray(1, 2, 3, 4, 5), 2)
+            .ToList()
+            .Should()
+            .BeEquivalentTo(new[] { 4, 5, 1, 2, 3 }, opt => opt.WithStrictOrdering());
+    }
+    
+    [Test]
+    public void Case2()
+    {
+        Exercise61
+            .RotateRight(Exercise61.ListNode.FromArray(0, 1, 2), 4)
+            .ToList()
+            .Should()
+            .BeEquivalentTo(new[] { 2, 0, 1 }, opt => opt.WithStrictOrdering());
+    }
+    
+    [Test]
+    public void Case3()
+    {
+        Exercise61
+            .RotateRight(Exercise61.ListNode.FromArray(1, 2), 0)
+            .ToList()
+            .Should()
+            .BeEquivalentTo(new[] { 1, 2 }, opt => opt.WithStrictOrdering());
+    }
+    
+    [Test]
+    public void Case4()
+    {
+        Exercise61
+            .RotateRight(Exercise61.ListNode.FromArray(1, 2), 2)
+            .ToList()
+            .Should()
+            .BeEquivalentTo(new[] { 1, 2 }, opt => opt.WithStrictOrdering());
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise61.cs
@@ -0,0 +1,85 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 61. Rotate List
+/// </summary>
+/// <remarks>
+/// Given the head of a linked list, rotate the list to the right by k places.
+/// </remarks>
+public class Exercise61
+{
+    public static ListNode RotateRight(ListNode head, int k)
+    {
+        if (k == 0 || head?.next == null)
+        {
+            return head;
+        }
+        
+        var len = 0;
+        var current = head;
+        var reverseItems = new Stack<ListNode>();
+        while (current != null)
+        {
+            reverseItems.Push(current);
+            len++;
+            
+            current = current.next;
+        }
+
+        ListNode newHead = null;
+        ListNode prevHead = head;
+        var rotatesSteps = k % len;
+        
+        while (rotatesSteps > 0)
+        {
+            newHead = reverseItems.Pop();
+            newHead.next = prevHead;
+            prevHead = newHead;
+            rotatesSteps--;
+        }
+        
+        var tail = reverseItems.Pop();
+        tail.next = null;
+
+        return newHead ?? head;
+    }
+
+    public class ListNode {
+         public int val;
+             public ListNode next;
+             public ListNode(int val=0, ListNode next=null) {
+                 this.val = val;
+                 this.next = next;
+             }
+
+             public static ListNode FromArray(params int[] arr)
+             {
+                 var head = new ListNode(arr[0]);
+
+                 var current = head;
+                 for (int i = 1; i < arr.Length; i++)
+                 {
+                     current.next = new ListNode(arr[i]);
+                     current = current.next;
+                 }
+
+                 return head;
+             }
+
+             public List<int> ToList()
+             {
+                 var result = new List<int>();
+                 
+                 var current = this;
+                 while (current != null)
+                 {
+                     result.Add(current.val);
+                     current = current.next;
+                 }
+
+                 return result;
+             }
+     }
+}
+
+
