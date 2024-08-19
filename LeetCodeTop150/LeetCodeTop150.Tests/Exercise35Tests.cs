d5fcf6c83ad1e66a8cafedb786c99e6e3ccfa7f6
Козлов Роман <kozlov@moedelo.org>
2024-08-19 15:15:38 +0300

    35. Search Insert Position

---
+++ LeetCodeTop150/LeetCodeTop150.Tests/Exercise35Tests.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeTop150.Tests;
+
+[TestFixture]
+public class Exercise35Tests
+{
+    [Test]
+    public void Case1()
+    {
+        int[] nums = [1,3,5,6];
+        var target = 5;
+
+        Exercise35.SearchInsert(nums, target).Should().Be(2);
+    }
+
+    [Test]
+    public void Case2()
+    {
+        int[] nums = [1,3,5,6];
+        var target = 2;
+
+        Exercise35.SearchInsert(nums, target).Should().Be(1);
+    }
+
+    [Test]
+    public void Case3()
+    {
+        int[] nums = [1,3,5,6];
+        var target = 7;
+
+        Exercise35.SearchInsert(nums, target).Should().Be(4);
+    }
+
+    [Test]
+    public void Case4()
+    {
+        int[] nums = [5];
+        var target = 7;
+
+        Exercise35.SearchInsert(nums, target).Should().Be(1);
+    }
+
+    [Test]
+    public void Case5()
+    {
+        int[] nums = [5];
+        var target = 3;
+
+        Exercise35.SearchInsert(nums, target).Should().Be(0);
+    }
+}
---
+++ LeetCodeTop150/LeetCodeTop150/Exercise35.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeTop150;
+
+/// <summary>
+/// 35. Search Insert Position
+/// </summary>
+/// <remarks>
+/// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+/// You must write an algorithm with O(log n) runtime complexity.
+/// </remarks>
+public static class Exercise35
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        var max = nums.Length;
+        var min = 0;
+        int i = -1;
+        
+        do
+        {
+            i = (int)Math.Floor((max - min) / 2d) + min;
+
+            var current = nums[i];
+            if (current == target)
+            {
+                return i;
+            }
+
+            if (target < current)
+            {
+                if (max == i)
+                {
+                    return i;
+                }
+                
+                max = i;
+            }
+            else // current < target
+            {
+                if (min == i)
+                {
+                    return i + 1;
+                }
+                
+                min = i;
+            }
+        } while (max - min > 0);
+
+        return i;
+    }
+}
